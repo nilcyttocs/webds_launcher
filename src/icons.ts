4ac6a2a293756536b299c94a78a94a15bca47990
Scott Lin <scott.lin@tw.synaptics.com>
2022-02-07 19:47:08 -0800

    Refactor code

--- src/icons.ts
+++ src/icons.ts
@@ -1,22 +1,7 @@
 import { LabIcon } from '@jupyterlab/ui-components';
 
-import mostUsedSvg from '../style/icons/grade.svg';
-import viewListSvg from '../style/icons/view_list.svg';
-import viewModuleSvg from '../style/icons/view_module.svg';
 import webdsSvg from '../style/icons/finger-touch-screen-svgrepo-com.svg';
 
-export const mostUsedIcon = new LabIcon({
-  name: 'enhLauncher:most-used',
-  svgstr: mostUsedSvg
-});
-export const viewListIcon = new LabIcon({
-  name: 'enhLauncher:list',
-  svgstr: viewListSvg
-});
-export const viewModuleIcon = new LabIcon({
-  name: 'enhLauncher:module',
-  svgstr: viewModuleSvg
-});
 export const webdsIcon = new LabIcon({
   name: 'enhLauncher:webds',
   svgstr: webdsSvg
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,14 @@
-// Copyright (c) Jupyter Development Team.
-// Distributed under the terms of the Modified BSD License.
-
 import {
   ILabShell,
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+import { MainAreaWidget } from '@jupyterlab/apputils';
 
 import { ILauncher } from '@jupyterlab/launcher';
+
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
-
-import { IStateDB } from '@jupyterlab/statedb';
-
-import { ITranslator } from '@jupyterlab/translation';
 
 import { launcherIcon } from '@jupyterlab/ui-components';
 
@@ -24,99 +18,69 @@ import { ReadonlyPartialJSONObject } from '@lumino/coreutils';
 
 import { Widget } from '@lumino/widgets';
 
-import { EXTENSION_ID, Launcher, LauncherModel } from './launcher';
+import { Launcher, LauncherModel } from './launcher';
 
-/**
- * The command IDs used by the launcher plugin.
- */
 namespace CommandIDs {
   export const create = 'launcher:create';
 }
 
-/**
- * A service providing an interface to the the launcher.
- */
+namespace Private {
+  // eslint-disable-next-line prefer-const
+  export let id = 0;
+}
+
+const EXTENSION_ID = '@webds/launcher:plugin';
+
 const plugin: JupyterFrontEndPlugin<ILauncher> = {
-  activate,
   id: EXTENSION_ID,
-  requires: [ITranslator],
-  optional: [ILabShell, ICommandPalette, ISettingRegistry, IStateDB],
+  autoStart: true,
+  optional: [ILabShell, ISettingRegistry],
   provides: ILauncher,
-  autoStart: true
+  activate
 };
 
-/**
- * Export the plugin as default.
- */
-export default plugin;
-
-/**
- * Activate the launcher.
- */
 async function activate(
   app: JupyterFrontEnd,
-  translator: ITranslator,
   labShell: ILabShell | null,
-  palette: ICommandPalette | null,
   settingRegistry: ISettingRegistry | null,
-  state: IStateDB | undefined
 ): Promise<ILauncher> {
-  const { commands, shell } = app;
-  const trans = translator.load('jupyterlab');
+  console.log('JupyterLab extension @webds/launcher is activated!');
+
+  const {commands, shell} = app;
 
   let settings: ISettingRegistry.ISettings | undefined = undefined;
   if (settingRegistry) {
     try {
       settings = await settingRegistry.load(EXTENSION_ID);
     } catch (reason) {
-      console.log(`Failed to load settings for ${EXTENSION_ID}.`, reason);
+      console.log(`Failed to load settings for ${EXTENSION_ID}\n${reason}`);
     }
   }
 
-  const model = new LauncherModel(settings, state);
-
-  if (state) {
-    Promise.all([
-      state.fetch(`${EXTENSION_ID}:usageData`),
-      state.fetch(`${EXTENSION_ID}:viewMode`),
-      app.restored
-    ])
-      .then(([usage, mode]) => {
-        model.viewMode = (mode as any) || 'cards';
-        for (const key in usage as any) {
-          model.usage[key] = (usage as any)[key];
-        }
-      })
-      .catch(reason => {
-        console.error('Fail to restore launcher usage data', reason);
-      });
-  }
+  const model = new LauncherModel(settings);
 
   commands.addCommand(CommandIDs.create, {
-    label: trans.__('New Launcher'),
+    label: 'WebDS Launcher',
     execute: (args: ReadonlyPartialJSONObject) => {
-      const cwd = args['cwd'] ? String(args['cwd']) : '';
       const id = `launcher-${Private.id++}`;
+      const cwd = args['cwd'] ? String(args['cwd']) : '';
       const callback = (item: Widget): void => {
-        shell.add(item, 'main', { ref: id });
+        shell.add(item, 'main', {ref: id});
       };
-      const launcher = new Launcher({ model, cwd, callback, commands });
 
+      const launcher = new Launcher({commands, model, cwd, callback});
       launcher.model = model;
+      launcher.title.label = 'Launcher';
       launcher.title.icon = launcherIcon;
-      launcher.title.label = trans.__('Launcher');
 
-      const main = new MainAreaWidget({ content: launcher });
-
-      // If there are any other widgets open, remove the launcher close icon.
-      main.title.closable = !!toArray(shell.widgets('main')).length;
+      const main = new MainAreaWidget<Launcher>({content: launcher});
       main.id = id;
+      main.title.closable = !!toArray(shell.widgets('main')).length;
 
-      shell.add(main, 'main', { activate: args['activate'] as boolean });
+      shell.add(main, 'main', {activate: args['activate'] as boolean});
 
       if (labShell) {
         labShell.layoutModified.connect(() => {
-          // If there is only a launcher open, remove the close icon.
           main.title.closable = toArray(labShell.widgets('main')).length > 1;
         }, main);
       }
@@ -125,23 +89,7 @@ async function activate(
     }
   });
 
-  if (palette) {
-    palette.addItem({
-      command: CommandIDs.create,
-      category: trans.__('Launcher')
-    });
-  }
-
   return model;
 }
 
-/**
- * The namespace for module private data.
- */
-namespace Private {
-  /**
-   * The incrementing id used for launcher widgets.
-   */
-  // eslint-disable-next-line prefer-const
-  export let id = 0;
-}
+export default plugin;
