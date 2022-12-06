c09b47077d603075f0b9ecd1b95f5b2a96edc449
Scott Lin <scott.lin@tw.synaptics.com>
2022-12-06 10:36:21 +0800

    Remove await for WebDS Service

--- src/index.ts
+++ src/index.ts
@@ -84,10 +84,6 @@ async function activate(
 ): Promise<ILauncher> {
   console.log("JupyterLab extension @webds/launcher is activated!");
 
-  if (service) {
-    await service.initialized;
-  }
-
   const { commands, shell } = app;
 
   let settings: ISettingRegistry.ISettings | undefined = undefined;
